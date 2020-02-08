0e4b143b9a8ff5f183ff36f3ca8f090a09790bee
Ivailo Alexandrov <ivailo.alexandrov@icloud.com>
2020-02-08 12:01:40 +0200

    init

---
+++ Tabbed App Example/ThirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdViewController.swift
+//  Tabbed App Example
+//
+//  Created by Ivailo Alexandrov on 8.02.20.
+//  Copyright © 2020 Ivailo Alexandrov. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
