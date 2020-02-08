249384fc1c56048f6cd4a26b37dd0459c3e8fbe0
Ivailo Alexandrov <ivailo.alexandrov@icloud.com>
2020-02-08 11:23:55 +0200

    Initial Commit

---
+++ Tabbed App Example/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Tabbed App Example
+//
+//  Created by Ivailo Alexandrov on 8.02.20.
+//  Copyright © 2020 Ivailo Alexandrov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
